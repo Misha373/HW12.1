568e3df6618cfeb2fd786c8d91f8423ba8e692f0
Misha373 <misasapka1@gmail.com>
2024-10-02 18:41:48 +0300

    project is are ready

--- HW12.1/Program.cs
+++ HW12.1/Program.cs
@@ -4,6 +4,23 @@
     {
         static void Main(string[] args)
         {
+            Console.OutputEncoding = System.Text.Encoding.UTF8;
+            Console.Write("Введіть номер порядку, Числа Фіабанічі якого ви хочете дізнатись: ");
+            int Num = int.Parse(Console.ReadLine());
+            int result = NumPhibanachi(Num);
+            Console.WriteLine($"Число Фібаначі з порядковим номеров - {Num}, дорівнює - {result}.");
+        }
+        public static int NumPhibanachi(int n)
+        {
+            if (n == 1)
+            {
+                return 0;
+            }
+            if (n == 2)
+            {
+                return 1;
+            }
+            return NumPhibanachi(n - 1) + NumPhibanachi(n - 2);
         }
     }
 }
